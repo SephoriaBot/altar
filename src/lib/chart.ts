27a8d06de1e77f2b0c3b31a3c7a7c2919dff3dec
SephoriaBot <withallofthisimdone@gmail.com>
2026-09-22 17:35:19 -0400

    Update chart.ts

--- src/lib/chart.ts
+++ src/lib/chart.ts
@@ -1,5 +1,5 @@
-import { getPlanetPositions, type PlanetPlacement } from './positions';
-import { getAngles, getHouseCusps, assignHouse, type HouseSystem, type Angles } from './houses';
+import { getPlanetPositions, type PlanetPlacement } from './positions.ts';
+import { getAngles, getHouseCusps, assignHouse, type HouseSystem, type Angles } from './houses.ts';
 
 export interface ChartPlacement extends PlanetPlacement {
   body: string;
